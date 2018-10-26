c25bcc64cc5e79563d73c6f5492ff3490a24e45a
Pedro Gomes <pdcgomes@gmail.com>
2018-10-26 13:43:15 +0100

    Initial Commit

---
+++ TinyCity/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  TinyCity
+//
+//  Created by Pedro Gomes on 26/10/2018.
+//  Copyright © 2018 Pedro Gomes. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
